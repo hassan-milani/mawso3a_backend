7ead3294c08d08ecab0bc54fdc7643997cdbceec
hassan-milani <hassanmilani2002@gmail.com>
2024-10-21 19:24:33 +0300

    refactor: paginate = 20

--- mawso3a_backend/settings.py
+++ mawso3a_backend/settings.py
@@ -45,7 +45,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
     
 }
 
